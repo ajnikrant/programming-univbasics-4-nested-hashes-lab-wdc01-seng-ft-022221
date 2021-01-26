2fb0f363eefe1dccbb65e028031041c60f906bcf
Amanda Nikrant <ajnikrant@gmail.com>
2021-01-26 20:09:41 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -41,6 +41,8 @@ def alan_kay_is_known_for
 programmer_hash[:alan_kay][:known_for]
 end
 
+
+
 def dennis_ritchies_language
 	programmer_hash =
  		{
@@ -57,7 +59,7 @@ def dennis_ritchies_language
         :languages => ["C"]
       }
     }
-
+programmer_hash[:dennis_ritchie][:languages]
 end
 
 def adding_matz
